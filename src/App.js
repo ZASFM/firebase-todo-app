fe9cd17dceb4a97558b1325b30991d312d0fd155
Mohammad Shafi <shafi.bahrami.2015@gmail.com>
2022-11-23 20:48:46 +0100

    Styling added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import {AiOutlinePlus} from 'react-icons/ai'
 import Todo from './Todo';
 
 const style={
-  bg:`h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CBE50]`
+  bg:`h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CBE50]`,
+  container: `bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4`,
+  heading: `text-3xl font-bold text-center text-gray-800 p-2`,
+  form: `flex justify-between`,
+  input: `border p-2 w-full text-xl`,
+  button: `border p-4 ml-2 bg-purple-500 text-slate-100`,
+  count: `text-center p-2`,
 }
 
 const App=()=>{
@@ -11,7 +17,7 @@ const App=()=>{
    return (
     <div className={style.bg}>
       <div className={style.container}>
-        <h3 className={style.header}>Todo app</h3>
+        <h3 className={style.heading}>Todo app</h3>
         <form className={style.form}>
           <input type="text" placeholder='Add a todo' className={style.input}/>
           <button className={style.button}><AiOutlinePlus size={30}/></button>
--- src/Todo.js
+++ src/Todo.js
@@ -2,7 +2,12 @@ import React from "react";
 import {FaRegTrashAlt} from 'react-icons/fa'
 
 const style={
-
+   li: `flex justify-between bg-slate-200 p-4 my-2 capitalize`,
+   liComplete: `flex justify-between bg-slate-400 p-4 my-2 capitalize`,
+   row: `flex`,
+   text: `ml-2 cursor-pointer`,
+   textComplete: `ml-2 cursor-pointer line-through`,
+   button: `cursor-pointer flex items-center`,
 }
 
 const Todo=(props)=>{
